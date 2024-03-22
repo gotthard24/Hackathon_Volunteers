2084131994939b45d733f62ce8d31128a79dbf6e
flet1234 <flet1908@gmail.com>
2024-03-22 14:09:30 +0200

    Added calculation func

--- get_city_from_api.py
+++ get_city_from_api.py
@@ -7,14 +7,13 @@ USERNAME = 'postgres'
 PASSWORD = 'rfrfirf1234'
 DATABASE = 'Israel_citys'
 
-# URL базового запроса к API GeoNames для поиска городов
+# URL
 base_url = 'http://api.geonames.org/searchJSON'
-# Параметры запроса для поиска всех городов Израиля
+# Params iwth only Israely places
 params = {
-    'country': 'IL',    # Код страны Израиль (IL)
-    'featureCode': 'PPL',  # Код фичи (PPL) обозначает город
-    'maxRows': 1000,     # Максимальное количество результатов (максимум 1000)
-    'username':'flet1234',  # Ваш пользовательский API ключ GeoNames
+    'country': 'IL',    
+    'maxRows': 1000,    
+    'username':'flet1234', 
     'startRow': 0
 }
 
@@ -27,24 +26,45 @@ while True:
     if response.status_code == 200:
         data = response.json()
 
-        # Вставка данных о городах в таблицу
-        for city in data['geonames']:
-            name = city['name']
-            latitude = city['lat']
-            longitude = city['lng']
-            insert_query = 'INSERT INTO israel_citys (name, latitude, longitude) VALUES (%s, %s, %s)'
-            cursor.execute(insert_query, (name, latitude, longitude))
-            connection.commit()
-
-        # Проверка, есть ли еще страницы результатов
-        if len(data['geonames']) < 1000:
-            break  # Выход из цикла, если это последняя страница результатов
+        # Insert and filter via repeated names, and population
+        if 'geonames' in data:
+            for city in data['geonames']:
+                search_query = f'SELECT name FROM israel_citys WHERE name = %s'
+                cursor.execute(search_query, (city['name'],))
+                result=cursor.fetchone()
+                if result is None:
+                    if city['population']>0:
+                        name = city['name']
+                        latitude = city['lat']
+                        longitude = city['lng']
+                        insert_query = 'INSERT INTO israel_citys (name, latitude, longitude) VALUES (%s, %s, %s)'
+                        cursor.execute(insert_query, (name, latitude, longitude))
+                        connection.commit()
+                    else:
+                        continue
+                else:
+                    continue
         else:
-            params['startRow'] += 1000  # Увеличение начальной строки для следующего запроса
+            break
+
+        # Checking if we have more variants
+        if len(data['geonames']) < 1000:
+            break  # If not
+        else:
+            params['startRow'] += 1000  # next page
     else:
         print('Error:', response.status_code)
         break
 
-# Закрытие соединения с базой данных
+# Closing connection
 cursor.close()
 connection.close()
+
+# # Test cases for me>>>
+# response = requests.get(base_url, params=params)
+# data = response.json()
+# print(data['geonames'])
+# # for x in data['geonames']:
+# #     print(x['name'])
+# #     print(x['lng'])
+# #     print(x['lat'])
