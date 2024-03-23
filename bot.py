4d7d6c47021469de728e7269d81ea8cf71b71674
Daniel <danya.smile@gmail.com>
2024-03-23 23:03:20 +0200

    working bot 1.0

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
 from telebot import types
 from telebot.types import ReplyKeyboardRemove
+import bot_functions
+import organisations as o
 
 TOKEN = "7044408816:AAE-A9JwCxXjZkjuFRAXfE9cmx9AomdloP0"
 
@@ -31,16 +33,43 @@ def volunteer_handler(message):
     bot.send_message(message.chat.id, "Please enter your city:")
     user_states[message.chat.id] = 'wait_for_input'
 
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'wait_for_input' or user_states.get(message.chat.id) == 'multiple_choice')
+def handle_user_input(message):
+    user_city = bot_functions.city_input(message.text)  
+    if user_states.get(message.chat.id) == 'multiple_choice':
+        city = o.precise_select_city(user_city)
+    else:
+        city = o.select_city(user_city)
+    if city == None:
+        bot.send_message(message.chat.id, f"Oops, city {user_city} not found\n Maybe you meant any of this?\n")
+        user_states[message.chat.id] = "wait_for_input" 
+        query_parts = [user_city[i:i+3] for i in range(0, len(user_city), 3)]
+        for part in query_parts:
+            city = o.select_city(part)
+            if city != None:
+                bot.send_message(message.chat.id, city) 
+    elif len(city) > 1:
+        bot.send_message(message.chat.id, f"There are more then one\n You meant any of this? \n{city}") 
+        user_states[message.chat.id] = "multiple_choice" 
+    else:     
+        del user_states[message.chat.id]
+        bot.send_message(message.chat.id,"One minute, Your request is being processed ")
+        user_states[message.chat.id] = "cc"  
+        global cities
+        cities = bot_functions.final_city_list_generation(user_city)
+        handler_correct_input(message)   
+  
+@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'cc')
+def handler_correct_input(message):
+    del user_states[message.chat.id]
+    user_states_btns[message.chat.id] = "Volunteer"
+    bot.send_message(message.chat.id, "Request completed")
+    bot.send_message(message.chat.id, "Choose interesting volunteer option for you:", reply_markup=topic_buttons())
+    
 @bot.message_handler(func=lambda message: message.text == "Donate")
 def donate_handler(message):
     user_states_btns[message.chat.id] = 'Donate'
     bot.send_message(message.chat.id, "Choose interesting donation option for you:", reply_markup=topic_buttons())
-
-@bot.message_handler(func=lambda message: user_states.get(message.chat.id) == 'wait_for_input')
-def handle_user_input(message):
-    bot.send_message(message.chat.id, f"You entered: {message.text}")
-    bot.send_message(message.chat.id, "Choose interesting volunteer option for you:", reply_markup=topic_buttons())
-    del user_states[message.chat.id]
     
 def topic_buttons():
     keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
@@ -51,14 +80,28 @@ def topic_buttons():
     keyboard.add(military_btn, health_btn, social_btn, animals_btn)
     return keyboard
 
-@bot.message_handler(func=lambda message: message.text == "Military")
-def military_handler(message):
+def result_by_btn_state_check(message):
     user_state_btns = user_states_btns.get(message.chat.id)
     if user_state_btns == 'Volunteer':
-        bot.send_message(message.chat.id, "Before was Volunteer")
+        bot_functions.print_volunteer_result(bot, message.chat.id, o.select_volunteers(cities, message.text))
     elif user_state_btns == 'Donate':
-        bot.send_message(message.chat.id, "Before was pressed Donate")
-    
+        bot_functions.print_donate_result(bot, message.chat.id, o.select_donate(message.text))
+
+@bot.message_handler(func=lambda message: message.text == "Military")
+def military_handler(message):
+    result_by_btn_state_check(message)
+        
+@bot.message_handler(func=lambda message: message.text == "Health")
+def health_handler(message):
+    result_by_btn_state_check(message)
+        
+@bot.message_handler(func=lambda message: message.text == "Social")
+def social_handler(message):
+    result_by_btn_state_check(message)
+        
+@bot.message_handler(func=lambda message: message.text == "Animals")
+def animals_handler(message):
+    result_by_btn_state_check(message)
 
 if __name__ == '__main__':
     bot.infinity_polling()
